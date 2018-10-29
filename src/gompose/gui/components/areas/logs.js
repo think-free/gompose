4f14697854fcec7f4e6852934367ca2f7142d4de
Think-Free <meumeu1402@gmail.com>
2018-10-29 10:15:35 +0100

    Better log page

--- src/gompose/gui/components/areas/logs.js
+++ src/gompose/gui/components/areas/logs.js
@@ -4,6 +4,35 @@ import { setValue } from '../store.js'
 import ToolBar from '../projecttoolbar.js'
 import Container from '../container.js'
 
+const layoutStyle = {
+    display: 'block',
+    height: 'calc(100% - 30px)',
+    width: '100%'
+}
+
+const tbcontainer = {
+    position: 'relative',
+    float: 'right',
+    textAlign: 'center',
+    top: 0,
+    height: '1.2em',
+    right: 30,
+    padding: '0.1em',
+    cursor: 'pointer'
+}
+
+const button = {
+    width: 65,
+    height: '1em',
+    textAlign: 'center',
+    padding: '10px',
+    color: "#E65D1E",
+    fontVariant: 'small-caps',
+    textTransform: 'uppercase',
+    userSelect:'none'
+}
+
+
 const title_container = {
     position: 'relative',
     textAlign: 'center',
@@ -15,11 +44,20 @@ const title_container = {
     fontVariant: 'small-caps',
     textTransform: 'uppercase',
     fontWeight: 'bold',
-    color: "#E65D1E"
+    color: "#E65D1E",
+    userSelect:'none'
+}
+
+const logStyle = {
+    display: 'block',
+    height: 'calc(100% - 30px)',
+    marginTop: 30,
+    width: '100%',
+    overflowY: 'auto'
 }
 
 const log_container = {
-    top: 70,
+    //top: 70,
     display: 'block',
     position: 'relative',
     padding: '10px',
@@ -36,7 +74,8 @@ class Logs extends React.Component {
             log : ""
         };
 
-        this.buttonClick=this.buttonClick.bind(this);
+        this.refreshClick=this.refreshClick.bind(this);
+        this.backClick=this.backClick.bind(this);
         this.getData=this.getData.bind(this);
     }
 
@@ -52,10 +91,19 @@ class Logs extends React.Component {
         fetch(url)
         .then(response => response.text())
         .then(data => this.setState({ log: data }))
+        .then(this.scrollToBottom())
     }
 
-    buttonClick(e) {
-        this.props.dispatch(setValue("Logs", "clicked"));
+    refreshClick(e) {
+        this.getData();
+    }
+
+    backClick(e) {
+        this.props.dispatch(setValue("logs", "none"));
+    }
+
+    scrollToBottom = () => {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
     }
 
     render() {
@@ -65,10 +113,16 @@ class Logs extends React.Component {
         var container = this.props.container != "none" ? this.props.container : ""
 
         return (
-             <div>
-                 <ToolBar backBt={true} isLog={true} />
+             <div style={layoutStyle}>
+                 <Container style={tbcontainer}> <span style={button} onClick={this.refreshClick}>Refresh</span> | <span style={button} onClick={this.backClick}>Back</span> </Container>
                  <Container style={title_container}>{parent} {project} {container}</Container>
-                 <pre style={log_container}>{this.state.log}</pre>
+
+                 <div style={logStyle}>
+                     <pre style={log_container}>
+                            {this.state.log} <br />
+                            <div style={{ float:"left", clear: "both" }} ref={(el) => { this.messagesEnd = el; }} />
+                     </pre>
+                 </div>
              </div>
         );
     }
